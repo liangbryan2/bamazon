3effd68806eba017cbede8d141a8d134ada9ca59
Onion Rings <liangbryan2@gmail.com>
2018-09-03 22:52:40 -0700

    add inquirer, mysql, table, show table, create connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,29 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 var Table = require('cli-table');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+});
+
+
+connection.query('SELECT * FROM products', function (err, res) {
+    var table = new Table({
+        head: ['Item ID', 'Product Name', 'Department', 'Price', 'Stock']
+    });
+
+    for (var i = 0; i < res.length; i++) {
+        table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+    }
+    console.log(table.toString());
+    connection.end();
+})
+
